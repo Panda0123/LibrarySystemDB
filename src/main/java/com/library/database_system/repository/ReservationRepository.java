41cb9fbeb98e6ee106ee4725eb1e465ba1c11d32
jamsedcabili@gmail.com <jamsedcabili@gmail.com>
2021-04-19 21:26:31 +0800

    Added: custom query in ReservationRepository
    Retrieve copies that are reserved now (current date)

--- src/main/java/com/library/database_system/repository/ReservationRepository.java
+++ src/main/java/com/library/database_system/repository/ReservationRepository.java
@@ -1,14 +1,13 @@
 package com.library.database_system.repository;
 
+import com.library.database_system.domain.BookCopy;
 import com.library.database_system.domain.Reservation;
-import com.library.database_system.dtos.ReservationDTO;
 import com.library.database_system.projections.ReservationProj;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -16,4 +15,7 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     @Query(value = "SELECT reservation FROM Reservation reservation")
     Collection<ReservationProj> findAllReservation();
     Optional<ReservationProj> findReservationById(Long id);
+
+    @Query(value = "SELECT reservation.bookCopy FROM Reservation reservation WHERE reservation.date = current_date() AND reservation.bookCopy.status = 'Available'")
+    Collection<BookCopy> updateBookCopyForReservation();
 }
